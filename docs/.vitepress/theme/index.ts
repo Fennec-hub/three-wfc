696fc83da319cf4910550b6aa093ef9c92ef4b86
Fennec <fennec@sahara.com>
2025-05-02 00:42:32 +0100

    chore(dev): Add FloatingVue tooltip and icon support to the editor

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,17 +1,21 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
-import './style.css'
+import { h } from "vue";
+import { type Theme } from "vitepress";
+import DefaultTheme from "vitepress/theme";
+import { OhVueIcon } from "./icons";
+import FloatingVue from "floating-vue";
+import "./styles/global.css";
+import "floating-vue/dist/style.css";
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
+    });
   },
   enhanceApp({ app, router, siteData }) {
-    // ...
-  }
-} satisfies Theme
+    app.component("v-icon", OhVueIcon);
+    app.use(FloatingVue);
+  },
+} satisfies Theme;
