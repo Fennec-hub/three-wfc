8265ef7d85a3a0d0da20005c2c81343431a87ac0
Fennec <fennec@sahara.com>
2025-05-02 00:33:57 +0100

    chore(dev): add eslintPlugin for the doc

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,23 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import eslintPlugin from "vite-plugin-eslint";
 
 export default defineConfig({
-  plugins: [dts({ insertTypesEntry: true, entryRoot: "lib/index.ts" })],
+  plugins: [
+    dts({ insertTypesEntry: true }),
+    eslintPlugin({
+      include: [
+        "src/**/*.{js,jsx,ts,tsx,vue}",
+        "docs/.vitepress/**/*.{js,ts,vue}",
+      ],
+      // cache: false, // Disable cache during debugging if needed
+      // lintOnStart: true,
+      // emitWarning: true,
+      // emitError: true,
+      // failOnError: false, // Don't fail build on ESLint errors
+    }),
+  ],
   build: {
     sourcemap: true,
     lib: {
